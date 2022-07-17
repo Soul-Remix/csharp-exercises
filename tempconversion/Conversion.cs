c46d30c4c06768a3cf58a6923b94b846ad4eace0
Soul-Remix <soulremix-dev@pm.me>
2022-07-17 13:10:14 +0300

    temperature conversion

---
+++ tempconversion/Conversion.cs
@@ -0,0 +1,18 @@
+namespace TempConversion;
+
+class Conversion
+{
+    public static double CelsiusToFahrenheit(string temperatureCelsius)
+    {
+        double celsius = Double.Parse(temperatureCelsius);
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        return fahrenheit;
+    }
+
+    public static double FahrenheitToCelsius(string temperatureFahrenheit)
+    {
+        double fahrenheit = Double.Parse(temperatureFahrenheit);
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        return celsius;
+    }
+}
---
+++ tempconversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TempConversion;
+
+class Program
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Choose conversion type: (1 or 2)");
+        Console.WriteLine("1. Celsius to Fahrenheit");
+        Console.WriteLine("2. Fahrenheit to Celsius");
+        string type = Console.ReadLine();
+
+        switch (type)
+        {
+            case "1":
+                Console.Write("Enter Celsius Temperature: ");
+                double fahrenheitTemp = Conversion.CelsiusToFahrenheit(Console.ReadLine() ?? "0");
+                Console.WriteLine($"Fahrenheit temperature is: {fahrenheitTemp}");
+                break;
+            case "2":
+                Console.Write("Enter Fahrenheit Temperature: ");
+                double celsiusTemp = Conversion.FahrenheitToCelsius(Console.ReadLine() ?? "0");
+                Console.WriteLine($"Celsius temperature is: {celsiusTemp}");
+                break;
+            default:
+                Console.WriteLine("Please select a convertor.");
+                break;
+        }
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadLine();
+    }
+
+}
