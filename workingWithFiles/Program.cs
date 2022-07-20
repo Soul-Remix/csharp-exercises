81943fc00ab2495829d673b5f9c43bdb2b14fbbb
Soul-Remix <soulremix-dev@pm.me>
2022-07-20 14:31:14 +0300

    working with files

---
+++ workingWithFiles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+
+namespace WorkingWithFiles;
+
+class MyProgram
+{
+    static void Main()
+    {
+        string thisDirectory = Directory.GetCurrentDirectory();
+        string storesPath = Path.Combine(thisDirectory, "stores");
+        string salesTotalPath = Path.Combine(thisDirectory, "salesTotalDir");
+
+
+        var files = GetFiles(storesPath);
+
+        var salesTotal = ClaculateSalesTotal(files);
+
+        if (!Directory.Exists(salesTotalPath))
+        {
+            Directory.CreateDirectory(salesTotalPath);
+        }
+
+        File.AppendAllText(Path.Combine(salesTotalPath, "salesTotal"), $"{salesTotal} {Environment.NewLine}");
+    }
+
+    public static IEnumerable<string> GetFiles(string path)
+    {
+        IEnumerable<string> allFiles = Directory.EnumerateFiles(path, "", SearchOption.AllDirectories);
+        List<string> files = new List<string>();
+
+        foreach (string file in allFiles)
+        {
+            if (Path.GetExtension(file) == ".json")
+            {
+                files.Add(file);
+            }
+        }
+
+        return files;
+    }
+
+    public static decimal ClaculateSalesTotal(IEnumerable<string> files)
+    {
+        decimal salesTotal = 0;
+
+        foreach (string file in files)
+        {
+            string jsonString = File.ReadAllText(file);
+            SalesTotal? sales = JsonSerializer.Deserialize<SalesTotal>(jsonString);
+            salesTotal += sales?.Total ?? 0;
+        }
+
+        return salesTotal;
+    }
+
+}
+
+record SalesTotal
+{
+    public decimal Total { get; set; }
+}
